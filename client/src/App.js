5aaa2b06f79ae25c7f7cca7e1a17ff9f6dfc77ac
Dylan Travis <dylan.travis23@outlook.com>
2021-07-22 18:05:52 -0700

    landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Landing from "./components/Landing"
 import Projects from "./components/Projects"
 import About from "./components/About"
 import Tech from "./components/Tech"
 
 const App = () => {
+  
 
   return(
     <div>
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,22 @@
 import React from "react";
+import avatar from "../img/avatar.png"
 
 const Landing = () => {
 
   return (
-    <div>
-      Landing
+    <div className="landing-container">
+      <div className="landing-avatar">
+        <img className="avatar"src={avatar}/>
+      </div>
+      <div className="landing-text">
+        <h1>Hi I'm Dylan...</h1>
+        <h4>And blah blah blah blah blah blah blah blah blah blah blah blah</h4>
+        <h4>And blah blah blah blah blah blah blah blah blah blah blah blah</h4>
+        <h4>And blah blah blah blah blah blah blah blah blah blah blah blah</h4>
+      </div>
+      <div className="landing-icons">
+        
+      </div>
     </div>
   )
 }
