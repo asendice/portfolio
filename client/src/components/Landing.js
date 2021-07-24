e2e333dccf5cd52e7575f7731338f61f8c823235
Dylan Travis <dylan.travis23@outlook.com>
2021-07-23 18:30:31 -0700

    proj api call and cards

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,24 +1,53 @@
 import React from "react";
-import avatar from "../img/avatar.png"
-
+import avatar from "../img/avatar.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaFilePdf } from "react-icons/fa";
 const Landing = () => {
-
   return (
-    <div className="landing-container">
-      <div className="landing-avatar">
-        <img className="avatar"src={avatar}/>
+    <>
+      <div className="landing-container">
+        <div className="landing-avatar">
+          <img className="avatar" src={avatar} />
+        </div>
+        <div className="landing-text">
+          <h1>Hi I'm Dylan...</h1>
+          <h4>
+            And blah blah blah blah blah blah blah blah blah blah blah blah
+          </h4>
+          <h4>
+            And blah blah blah blah blah blah blah blah blah blah blah blah
+          </h4>
+          <h4>
+            And blah blah blah blah blah blah blah blah blah blah blah blah
+          </h4>
+        </div>
       </div>
-      <div className="landing-text">
-        <h1>Hi I'm Dylan...</h1>
-        <h4>And blah blah blah blah blah blah blah blah blah blah blah blah</h4>
-        <h4>And blah blah blah blah blah blah blah blah blah blah blah blah</h4>
-        <h4>And blah blah blah blah blah blah blah blah blah blah blah blah</h4>
+      <div className="icons">
+        <a
+          href="https://www.linkedin.com/in/dylan-travis-2716a4205/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://www.github.com/asendice"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://dtravmysite.s3.us-west-1.amazonaws.com/Dylan_Travis_-_Web_Developer.pdf"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <FaFilePdf />
+        </a>
       </div>
-      <div className="landing-icons">
-        
-      </div>
-    </div>
-  )
-}
+    </>
+  );
+};
 
 export default Landing;
--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import backendApi from "../apis/backendApi";
 
 const Projects = () => {
+  const [projects, setProjects] = useState([]);
+
+  const getProjects = async () => {
+    await backendApi
+      .get("/projects")
+      .then((response) => {
+        if (response) {
+          console.log(response, "response");
+          return response;
+        } else {
+          const error = new Error(
+            `Error ${response.status}: ${response.statusText}`
+          );
+          error.response = response;
+          throw error;
+        }
+      })
+      .then((response) => setProjects(response.data.message));
+  };
+
+  useEffect(() => {
+    getProjects();
+  }, []);
+
+  const renderProjectCards = () => {
+    return projects.map((proj) =>{
+      return (
+        <div key={proj.id} className="project-card">
+          <img src={proj.images[0]}/>
+          <h1>{proj.name}</h1>
+        </div>
+      )
+    })
+  }
+
+  console.log(projects)
 
   return (
-    <div>
-      Projects
+    <div className="projects-container">
+      <div className="section-title"> <h2> My Projects </h2> </div>
+      <div className="projects">
+        {renderProjectCards()}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Projects;
